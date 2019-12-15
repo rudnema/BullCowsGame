459e454eee011203826b426c0f33cbdce5b18d28
Julia Cieslik <rudnema@gmail.com>
2019-12-15 20:27:18 +0100

    Get guess and print back rename (ctrl r)

--- Section_02/BullCowsGame/main.cpp
+++ Section_02/BullCowsGame/main.cpp
@@ -4,51 +4,50 @@
 using namespace std;
 
 void PrintIntro();
+string GetGuessAndPrintBack();
 
 // the entry point for our application
 int main()
 {
 	PrintIntro();
+
+	GetGuessAndPrintBack();
 	
 
+	//delete
 	// get a guess from player
-	cout << "Enter your guess: ";
-	string Guess = "";
-	getline(cin, Guess);
-
-
+	//cout << "Enter your guess: ";
+	//(cin, Guess);
 	// repeat the guess back to them
-	cout << "Your guess was:" << Guess << endl;
-	
-
-
-	// get a guess from player
-	cout << "Enter your guess: ";
-
-	getline(cin, Guess);
-
-	// repeat the guess back to them
-	cout << "Your guess was:" << Guess << endl;
-
-
-
-
+	//cout << "Your guess was:" << Guess << endl;
 
 	cout << endl;
 	
 	// CZAS NA ODPOCZYNEK I PIZZE
 
-
-
 	return 0;
 
 }
+
+// introduce the game
 void PrintIntro() {
-	// introduce the game
+	
 	constexpr int WORLD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game";
 	cout << "Can you guess the " << WORLD_LENGTH;
 	cout << " letter isogram I'm thinking  of?\n";
 	cout << endl;
 	return;
+}
+// get a guess from player
+string GetGuessAndPrintBack() {
+	
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+
+	// print guess back
+	cout << "Your guess was:" << Guess << endl;
+	return Guess;
 }
