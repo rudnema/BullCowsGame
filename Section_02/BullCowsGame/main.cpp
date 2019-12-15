9f6f86953f4c78c73102dc643b148938521fd1f2
Julia Cieslik <rudnema@gmail.com>
2019-12-15 19:55:56 +0100

    To robi, dobrze, dobrze.

--- Section_02/BullCowsGame/main.cpp
+++ Section_02/BullCowsGame/main.cpp
@@ -3,28 +3,52 @@
 
 using namespace std;
 
+void PrintIntro();
+
+// the entry point for our application
 int main()
 {
-
-	// introduce the game
-	constexpr int WORLD_LENGTH = 9;
-
-	cout << "Welcome to Bulls and Cows, a fun word game";
-	cout << "Can you guess the " << WORLD_LENGTH;
-	cout << " letter isogram I'm thinking  of?\n";
-	cout << endl;
+	PrintIntro();
+	
 
 	// get a guess from player
 	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
+	getline(cin, Guess);
+
+
 	// repeat the guess back to them
-	cout << "Your guess wa:" << Guess << endl;
+	cout << "Your guess was:" << Guess << endl;
 	
+
+
+	// get a guess from player
+	cout << "Enter your guess: ";
+
+	getline(cin, Guess);
+
+	// repeat the guess back to them
+	cout << "Your guess was:" << Guess << endl;
+
+
+
+
+
 	cout << endl;
 	
 	// CZAS NA ODPOCZYNEK I PIZZE
 
+
+
 	return 0;
 
+}
+void PrintIntro() {
+	// introduce the game
+	constexpr int WORLD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking  of?\n";
+	cout << endl;
+	return;
 }
