157773eaccbb7f6489e698510401d644426dbcea
Julia Cieslik <rudnema@gmail.com>
2019-12-17 03:32:24 +0100

    learnig classes

---
+++ Section_02/BullCowsGame/FBullCowGame.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+
+class FBullCowGame {
+ public:
+	void Reset(); // TODO make a more rich return value
+	int GetMaxTries();
+	int GetCurrentTry;
+	bool IsGameWon();
+	bool CheckGuessValidity(string); // TODO make a more rich return value
+
+
+
+	// Please try and ignore this and focus on the interface aboce
+private:
+	int MyCurrentTry;
+	int MyMaxTries;
+};
